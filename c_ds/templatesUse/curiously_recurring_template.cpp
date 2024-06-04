4f2c6cc82d16fa164f73ec5d0b13f158e2cfe132
nitin.sharma <nitin.sharma@gemini.com>
2024-06-04 11:12:40 +0800

    adding some templates

---
+++ c_ds/templatesUse/curiously_recurring_template.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+
+class Appliction{
+    public:
+    Appliction(){
+        std::cout << "Appliction" << std::endl;
+    }
+};
+
+template <typename T>
+class CustomApp1 : public T{
+    public:
+    CustomApp1(){
+        std::cout << "CustomApp1" << std::endl;
+    }
+};
+
+template <typename T>
+class Level2App : public T{
+    public:
+    Level2App(){
+        std::cout << "Level2App" << std::endl;
+    }
+};
+
+
+
+
+void play_with_crtp_sample_1(){
+    Level2App<CustomApp1<Appliction>> level2AppObj;
+    std::cout << "play_with_crtp_sample_1" << std::endl;
+}
+
+void play_with_crtp_sample_1_test(){
+    class ApplicationMock{public:ApplicationMock(){std::cout << "this is in mock" << std::endl;}};
+    Level2App<CustomApp1<ApplicationMock>> level2AppObj;
+    std::cout << "play_with_crtp_sample_1_test" << std::endl;
+}
+template <typename T>
+class Level2Type2App : public T<Level2Type2App> {
+    public:
+    Level2App(){
+        std::cout << "Level2App" << std::endl;
+    }
+};
+int main(){
+    play_with_crtp_sample_1();
+    play_with_crtp_sample_1_test();
+    return 0;
+}
