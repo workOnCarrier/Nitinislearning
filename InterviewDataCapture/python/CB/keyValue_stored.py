47f5f582ebc8698623862e8ea07d808ae60388fe
nitinsharma <workonideas@gmail.com>
2025-09-30 19:21:49 +0800

    with historical data snapshot

--- InterviewDataCapture/python/CB/keyValue_stored.py
+++ InterviewDataCapture/python/CB/keyValue_stored.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 class KeyValueStore:
     def __init__(self):
         self.store = defaultdict(dict)
+        self.purged_store = defaultdict(dict)
 
     def set(self, timestamp, key, field, value, ttl = None):
         data = self.store[key]
@@ -17,6 +18,7 @@ class KeyValueStore:
             if value_tuple[2] is not None and timestamp >= value_tuple[1] + value_tuple[2]:
                 keys_to_remove.append(field)
         for field in keys_to_remove:
+            self.purged_store[key][field] = data[field]
             del data[field]
 
     def get(self, timestamp, key, field):
@@ -34,6 +36,7 @@ class KeyValueStore:
             return False
         self.remove_ttl_expired(timestamp, key)
         if key in data.keys():
+            self.purged_store[key][field] = data[field]
             del data[key]
             return True
         return False
@@ -41,7 +44,6 @@ class KeyValueStore:
     def compare_and_set(self, timestamp, key, field, expected_value, new_value, ttl = None):
         data = self.store.get(key, None)
         if not data:
-            print(f"\t key:{key} not found")
             return False
         self.remove_ttl_expired(timestamp, key)
         if field in data.keys():
@@ -51,15 +53,24 @@ class KeyValueStore:
                 return True
         return False
 
-    def prefix_search(self, timestamp, key, field, prefix):
-        """Return a list of (key, value) pairs where the key starts with the prefix."""
+    def search(self, timestamp, key, field):
         result = []
         data = self.store.get(key, None)
         if not data:
             return result
         self.remove_ttl_expired(timestamp, key)
         if key in data.keys():
-            result  = [f"{k}({v})" for k, v in self.data.items() if k.startswith(prefix)]
+            result  = [f"{k}({v})" for k, v in sorted(data.items())]
+        return result
+ 
+    def prefix_search(self, timestamp, key, field, prefix):
+        result = []
+        data = self.store.get(key, None)
+        if not data:
+            return result
+        self.remove_ttl_expired(timestamp, key)
+        if field in data.keys():
+            result  = [f"{k}({v})" for k, v in sorted(data.items()) if k.startswith(prefix)]
         return result
     
     def set_with_ttl(self, timestamp, key, field, value, ttl):
@@ -68,7 +79,18 @@ class KeyValueStore:
     def compare_and_set_with_ttl(self, timestamp, key, field, expected_value, new_value, ttl ):
         self.compare_and_set(timestamp, key, field, expected_value, new_value, ttl)
     
+    def search_historical_at_timestamp(self, timestamp, key, field, historical_ts):
+        result = []
+        historical_data = self.purged_store.get(key, None)
+        if historical_data:
+            result  = [f"{k}({v})" for k, v in sorted(historical_data.items()) if v[1] <= historical_ts]
 
+        data = self.store.get(key, None)
+        if field in data.keys():
+            result  += [f"{k}({v})" for k, v in sorted(data.items()) if v[1] <= historical_ts]
+
+        return result
+ 
 # Example usage:
 def test():
     db = KeyValueStore()
@@ -87,8 +109,25 @@ def test():
         print(f" expected get to fetch None")
     else:
         print(f" {expecting_None} ")
+    print(f"{'-'*10}")
 
+def test_historical():
+    db = KeyValueStore()
+    db.set(100, 'foo', 'bar', 1)
+    db.set_with_ttl(102, 'foo', 'bar_', 5, 10)
+    db.set_with_ttl(103, 'foo', 'bar', 6, 10)
+    get_val = db.get(104, 'foo', 'bar' )
+    if get_val != 1:
+        print(f" expected:{1} got:{get_val}")
+    search_before_ttl_expiry = db.search(111, 'foo', 'bar')
+    print(f" before ttl expiry:{search_before_ttl_expiry}")
+    search_after_ttl_expiry = db.search(112, 'foo', 'bar')
+    db.set(113, 'foo', '_bar_', 1)
+    print(f" after ttl expiry:{search_after_ttl_expiry}")
+    search_history = db.search_historical_at_timestamp(115, 'foo', 'bar', 110)
+    print(f" history ttl expiry:{search_history}")
 
 
 if __name__ == "__main__":
     test()
+    test_historical()
